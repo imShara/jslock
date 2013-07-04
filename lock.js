378941090ae22df1fc1577948d65ec1fbe0d3b19
Way, No <noway@2ch.hk>
2013-07-05 05:29:54 +1100

    removed trailing whitespaces

--- lock.js
+++ lock.js
@@ -47,10 +47,10 @@ w.lk = {
   onlyRusLang: false,
 
   sharing: {
-    // Адрес, который публикует пользователь в соцсетях якорь #block заставляет показать страницу блокировки в любом случае                         
+    // Адрес, который публикует пользователь в соцсетях якорь #block заставляет показать страницу блокировки в любом случае
     url: 'http://' + location.host + '/#block',
     // Заголовок поста, который публикует пользователь в соцсетях
-    title: 'Интернет-свобода под угрозой!', 
+    title: 'Интернет-свобода под угрозой!',
      // Текст, который публикует пользователь в соцсетях 140 символов
     text: '1 августа вступит в силу закон о борьбе с пиратским видео в интернете, который даёт возможность закрыть любой сайт.',
     // Ссылка на изображение, которое публикует пользователь в соцсетях
@@ -63,7 +63,7 @@ w.lk = {
     // Ссылка на сайт в заголовке
     siteurl:  'http://' + location.host + '/',
     // Надпись на кнопке голосования
-    votename: 'Подписать петицию',                  
+    votename: 'Подписать петицию',
     // Адрес петиции
     voteurl:  'http://www.onlinepetition.ru/%D0%BE%D1%82%D0%BC%D0%B5%D0%BD%D0%B8%D1%82%D1%8C-%D0%BF%D1%80%D0%B8%D0%BD%D1%8F%D1%82%D0%B8%D0%B5-%D0%B7%D0%B0%D0%BA%D0%BE%D0%BD%D0%BE%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B0---292521-6-/petition.html',
     // Основной текст страницы
@@ -310,7 +310,7 @@ lk.share = function(net) {
       break;
     }
     case "f": {
-      url  = 'http://www.facebook.com/sharer.php?s=100';   
+      url  = 'http://www.facebook.com/sharer.php?s=100';
       url += '&p[title]='     + toURI(lk.sharing.title);
       url += '&p[summary]='   + toURI(lk.sharing.text);
       url += '&p[url]='       + toURI(lk.sharing.url);
@@ -527,9 +527,9 @@ documentReady(function(){
 
   var style = d.createElement('style');
   style.type = 'text/css';
-  if (style.styleSheet) 
+  if (style.styleSheet)
     style.styleSheet.cssText = lk.css;
-  else 
+  else
     style.appendChild(d.createTextNode(lk.css));
   h.appendChild(style);
 
